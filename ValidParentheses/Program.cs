37781c7183a641ca3bf992ba431a0903828a003a
Cavin <cavinosullivan@hotmail.com>
2021-05-03 16:21:01 +0100

    initial commit

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        public static bool IsValid(string s)
+        {
+            var regex = new Regex(@"\((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*\)");
+            var match = regex.Match(s);
+
+            if (match.Success)
+                return true;
+            else
+                return false;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var boolean = IsValid("{[]}");
+
+            Console.WriteLine(boolean);
+        }
+    }
+}
